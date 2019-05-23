a5e01562eac9216314e53d81c6dfafd38208b7b2
SasakiTakanori <sasaki.takanori@imagica.jp>
2019-05-23 12:25:29 +0900

    期限日にNULLを設定できるように変更

--- html/new_todo_list.php
+++ html/new_todo_list.php
@@ -7,12 +7,22 @@ $user = "root";  // ユーザー名
 $pass = "password";  // ユーザー名のパスワード
 try {
     $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]);
-    $stmt =  $pdo->prepare('INSERT INTO todo_list (body, done, expiration, priority) VALUES(:body, :done, :expiration, :priority)');
-    $date = new DateTime($_POST['expiration']);
-    // var_dump($date->format('Y-m-d'));
+    $stmt =  $pdo->prepare('INSERT INTO todo_list (body, done, expiration, priority) VALUES(:body, :done, :expiration, :priority)');    $date = new DateTime($_POST['expiration']);
     $stmt->bindParam(':body', $_POST['body'], PDO::PARAM_STR);
     $stmt->bindParam(':done', $_POST['done'], PDO::PARAM_INT);
-    $stmt->bindParam(':expiration', $date->format('Y-m-d'), PDO::PARAM_STR);
+
+    // 期限日に関しては受け取ったパラメータをDBに保存できるフォーマットに編集して:expirationにbindParamします
+    $date = NULL;
+    if(! empty($_POST['expiration'])) {
+        $date = new DateTime($_POST['expiration']);
+        // var_dump($date->format('Y-m-d'));
+    }
+    if(is_null($date)){
+        $stmt->bindParam(':expiration', $date, PDO::PARAM_NULL);
+    }else{
+        $stmt->bindParam(':expiration', $date->format('Y-m-d'), PDO::PARAM_STR);
+    }
+
     $stmt->bindParam(':priority', $_POST['priority'], PDO::PARAM_INT);
     $stmt->execute();
     // リダイレクト処理
--- html/update_todo_list.php
+++ html/update_todo_list.php
@@ -9,11 +9,22 @@ try {
     $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]);
     $stmt =  $pdo->prepare('UPDATE todo_list SET body = :body, done = :done, expiration = :expiration, priority = :priority WHERE id = :id') ;
     $date = new DateTime($_POST['expiration']);
-    // var_dump($date->format('Y-m-d'));
     $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_STR);
     $stmt->bindParam(':body', $_POST['body'], PDO::PARAM_STR);
     $stmt->bindParam(':done', $_POST['done'], PDO::PARAM_INT);
-    $stmt->bindParam(':expiration', $date->format('Y-m-d'), PDO::PARAM_STR);
+
+        // 期限日に関しては受け取ったパラメータをDBに保存できるフォーマットに編集して:expirationにbindParamします
+    $date = NULL;
+    if(! empty($_POST['expiration'])) {
+        $date = new DateTime($_POST['expiration']);
+        // var_dump($date->format('Y-m-d'));
+    }
+    if(is_null($date)){
+        $stmt->bindParam(':expiration', $date, PDO::PARAM_NULL);
+    }else{
+        $stmt->bindParam(':expiration', $date->format('Y-m-d'), PDO::PARAM_STR);
+    }
+
     $stmt->bindParam(':priority', $_POST['priority'], PDO::PARAM_INT);
     $stmt->execute();
     // リダイレクト処理
